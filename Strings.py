35545c72814d2f7204ceb9201d29f5b648e280a3
Your Name <your.email@example.com>
2026-01-27 21:44:44 +0600

    Strings.py

---
+++ Strings.py
@@ -0,0 +1,17 @@
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+a = """EEE"""
+print(a)
+
+b = "Hello, World!"
+print(b[2:5]) #Get symbols from 2 position to 5(exclusively)
+
+a = "Hello, World!"
+print(a.upper()) #returns the string in upper cases and works the same for lower()
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c) #merges variables(concatenate).
