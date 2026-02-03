7e1d582f0e545c508d636e790d4c9fafed75415e
Your Name <your.email@example.com>
2026-02-03 18:33:30 +0600

    practice2

---
+++ practice2PP2/Boolean/boolean-values.py
@@ -0,0 +1,30 @@
+#1
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+#2
+print(bool("Hello"))
+print(bool(15))
+
+#3
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+#4
+def myFunction() :
+  return True
+
+print(myFunction())
+
+#5
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
