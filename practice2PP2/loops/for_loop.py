f7390607b7006c926590bc170d009760c48d1b76
Your Name <your.email@example.com>
2026-02-03 19:28:16 +0600

    practice2

---
+++ practice2PP2/loops/for_loop.py
@@ -0,0 +1,24 @@
+#1
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+#2
+for x in "banana":
+  print(x)
+
+#3
+for i in range(5):
+    print(i)
+
+
+#4
+for i in range(0,11,2):
+    print(i)
+
+
+#5
+total = 0
+for i in range(1,6):
+    total += i
+print(total)
